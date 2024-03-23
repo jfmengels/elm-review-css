5e6f7212779cb29268437f821ca989f2c523218b
Jeroen Engels <jfm.engels@gmail.com>
2024-03-23 22:33:46 +0100

    Init project

---
+++ tests/Css/NoUnknownClassesTest.elm
@@ -0,0 +1,31 @@
+module Css.NoUnknownClassesTest exposing (all)
+
+import Css.NoUnknownClasses exposing (rule)
+import Review.Test
+import Test exposing (Test, describe, test)
+
+
+all : Test
+all =
+    describe "Css.NoUnknownClasses"
+        [ test "should not report an error when REPLACEME" <|
+            \() ->
+                """module A exposing (..)
+a = 1
+"""
+                    |> Review.Test.run rule
+                    |> Review.Test.expectNoErrors
+        , test "should report an error when REPLACEME" <|
+            \() ->
+                """module A exposing (..)
+a = 1
+"""
+                    |> Review.Test.run rule
+                    |> Review.Test.expectErrors
+                        [ Review.Test.error
+                            { message = "REPLACEME"
+                            , details = [ "REPLACEME" ]
+                            , under = "REPLACEME"
+                            }
+                        ]
+        ]
